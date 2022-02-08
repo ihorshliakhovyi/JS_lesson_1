4d8bcae2106e093b7c3508f3913e4de78c53c60f
Ihor Shliakhovyi <ihorshliakhovyi@gmail.com>
2022-02-08 23:25:07 +0200

    Task6

--- js/index6.js
+++ js/index6.js
@@ -2,4 +2,22 @@
 
 let input;
 let total = 0;
+let message;
+
+
+while (input !== null) {
+    input = prompt('Введите число');
+    if (input === null) {
+        break;
+    }
+    if (Number.isNaN(parseFloat(input))) {
+        alert('Было введено не число, попробуйте еще раз');
+        input = 0;
+    }
+    total += parseFloat(input);
+}
+
+alert(`Общая сумма чисел равна ${total}`);
+
+
 
